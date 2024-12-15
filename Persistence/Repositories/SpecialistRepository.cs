0c50c65c0d366c302da374dac9be0b1c85cc30d9
Алексей Яблонский <savageseed@yandex.ru>
2024-12-16 02:46:24 +0500

    feat(controller and first repos):

---
+++ Persistence/Repositories/SpecialistRepository.cs
@@ -0,0 +1,20 @@
+﻿namespace Persistence.Repositories;
+
+public class SpecialistRepository : ISpecialistRepository
+{
+    public async Task GetAllSpecialistsAsync()
+    {
+        //
+    }
+
+    public async Task AddNewSpecialist()
+    {
+        //
+    }
+}
+
+public interface ISpecialistRepository
+{
+    Task GetAllSpecialistsAsync();
+    Task AddNewSpecialist();
+}
